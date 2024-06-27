6905c2e5cf20f32925904d9bc5e95ad641612083
olivierldn <33377197+olivierldn@users.noreply.github.com>
2024-06-27 20:23:17 +0000

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,30 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      // ...
+      keyframes: {
+        'slide-left': {
+          from: { transform: 'translateX(0)' },
+          to: { transform: 'translateX(-100%)' },
+        },
+      },
+      animation: {
+        'slide-left-infinite': 'slide-left 15s linear infinite',
+      },
+      height: {
+        "10v": "10vh",
+        "20v": "20vh",
+        "30v": "30vh",
+        "40v": "40vh",
+        "50v": "50vh",
+        "60v": "60vh",
+        "70v": "70vh",
+        "80v": "80vh",
+        "90v": "90vh",
+        "100v": "100vh",
+      },
+    },
   },
   plugins: [],
 }
-
